49de4586fa1d87d9918cb4d3d7744ba710a543c0
Mohini <mohini.s@kambala.co.in>
2022-10-27 16:19:11 +0530

    test

--- test_api.py
+++ test_api.py
@@ -8,12 +8,12 @@ logging.basicConfig(level=logging.DEBUG)
 api = NorenApiPy()
 
 #credentials
-user    = <uid>
-pwd     = <password>
-factor2 = <2nd factor>
-vc      = <vendor code>
-app_key = <secret key>
-imei    = <imei>
+user    = "POORNA"
+pwd     = "3906e2afbfffe683f5d80264e7e5c3511c18215beade570067f5d44d0e3bf7a4"
+factor2 = "AAAAA1234A"
+vc      = "NOREN_WEB"
+app_key = "1.0.9"
+imei    = "ag3tbbbb33"
 
 #make the api call
 ret = api.login(userid=uid, password=pwd, twoFA=factor2, vendor_code=vc, api_secret=app_key, imei=imei)
